7ac8db52e6b11d2f305485ef7d9ad35b34f1661d
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-08-19 22:32:38 -0300

    feat: ExpensiveController

---
+++ src/CashFlow.Comunication/Requests/RequestExpenseJson.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashFlow.Comunication.Requests;
+
+internal class RequestExpenseJson
+{
+}
---
+++ src/CashFlowApi/Controllers/ExpensesController.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Comunication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestExpenseJson request)
+    {
+        return Created();
+    }
+}
