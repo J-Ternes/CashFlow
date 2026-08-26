a1220bc1011178826cf9945ac5fd709b09c983e8
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-08-25 21:38:13 -0300

    Criando o Middleware

---
+++ src/CashFlowApi/Middleware/CultureMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace CashFlow.Api.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = new CultureInfo("en");
+
+        if(string.IsNullOrWhiteSpace(culture) == false)
+        {
+            cultureInfo = new CultureInfo(culture); //Sobrescreve a cultura padrão com a cultura do header
+        }
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+
+    
+}
